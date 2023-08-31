32f355a50b39ccdf603bbad3f262358999d3d5ed
Tuukka Pitkänen <tuksu090@hotmail.com>
2023-08-31 09:48:58 +0300

    Performance

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
+import { Text, View, TextInput, TouchableOpacity, KeyboardAvoidingView, SafeAreaView } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 /* import { StatusBar } from 'expo-status-bar'; */
 /* import app from '/components/firebaseConfig'; */
 import Menu from './components/menu';
-import { Text, View, TextInput, TouchableOpacity, KeyboardAvoidingView, SafeAreaView } from 'react-native';
+import words from './components/words';
 import styles from './components/styles';
-import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function App() {
-  const words = ['apple', 'baker', 'cabin', 'cable', 'daddy', 'dizzy', 'eagle', 'eager', 'early', 'faces', 'flame', 'fuzzy', 'grape', 'jazzy', 'jelly', 'kebab', 'latch', 'lunar', 'mirth', 'nudge', 'oasis', 'ocean', 'peach', 'pizza', 'plaza', 'quest', 'quiet', 'roast', 'saber', 'sable', 'squat', 'sugar', 'sweep', 'table', 'thorn', 'tiger', 'umbra', 'vague', 'vivid', 'vowel', 'waltz', 'wrist', 'xerox', 'yacht', 'yummy', 'zesty', 'zebra'];
 
   /* 
     IMPORTANT: six word words/letters cant be done, give the user a prompt that says word not availabe
@@ -16,16 +16,16 @@ export default function App() {
     Signup and Login System to save the progress (Not necessery) can add a login button that user can press it and login/signup (using firebase and config already done ./components/firebaseConfig.js)
     Ads to raise the points, 30sec = getWord + resetGame, and short = getWord (like 2 points) popup or when pressing and points are 0 get popup if user wants more
     Everytime user clicks a button remove certain point
-    Menu? Add github button? Credits?
+    Add github button? Credits?
   */
 
   const [backgroundColor, setBackgroundColor] = useState('white');
   const [darkmode, setDarkMode] = useState(false);
   const [showmenu, setShowmenu] = useState(false);
-  const [targetWord, setTargetWord] = useState(generateRandomWord());
   const [guess, setGuess] = useState('');
-  const [revealedLetters, setRevealedLetters] = useState([]);
   const [attempts, setAttempts] = useState(0);
+  const [targetWord, setTargetWord] = useState(generateRandomWord());
+  const [revealedLetters, setRevealedLetters] = useState([]);
   const [guessedLetters, setGuessedLetters] = useState(Array(6).fill(Array(5).fill('')));
 
   //Change background color from a button
---
+++ components/words.js
@@ -0,0 +1,29 @@
+const words = [
+    'apple', 
+    'baker', 
+    'cabin', 'cable', 
+    'daddy', 'dizzy', 
+    'eagle', 'eager', 'early', 
+    'faces', 'flame', 'fuzzy', 
+    'grape', 
+    'jazzy', 'jelly', 
+    'kebab', 
+    'latch', 'lunar', 
+    'mirth', 
+    'nudge', 
+    'oasis', 'ocean', 
+    'peach', 'pizza', 'plaza', 
+    'quest', 'quiet', 
+    'roast', 
+    'saber', 'sable', 'squat', 'sugar', 'sweep', 
+    'table', 'thorn', 'tiger', 
+    'umbra', 
+    'vague', 'vivid', 'vowel', 
+    'waltz', 'wrist', 
+    'xerox', 
+    'yacht', 
+    'yummy', 
+    'zesty', 'zebra'
+];
+
+export default words;
