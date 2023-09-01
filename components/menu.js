ff500a62b408b3a6325b4bee6e6da7fbcca34a98
Tuukka Pitkänen <tuksu090@hotmail.com>
2023-09-01 10:00:37 +0300

    Register, Login, Logout

--- components/menu.js
+++ components/menu.js
@@ -1,15 +1,10 @@
 import { Pressable, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { auth } from './firebaseConfig';
 import { getDatabase, ref, set } from 'firebase/database';
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
 
 /*
-    DATABASE IN USE: Firebase Database. (Config found in firebaseConfig.js)
-    User can use register after inputting Email and Password, if email is found send an error.
-    Give user points, score and level in firebase database: getWord points (4), resetGame points (2), score 0 and level starts 1
-    newTimer = when the word is completed save 12h to it after it hits 0 give user a new word.
-
     BUILD IN DATABASE:
     user.uid {
         email:
@@ -27,6 +22,17 @@ import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'fire
 const Menu = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((user) => {
+            setUser(user);
+        });
+
+        return () => {
+            unsubscribe();
+        };
+    }, []);
 
     const handleSignup = async () => {
         try {
@@ -63,29 +69,53 @@ const Menu = () => {
         }
     };
 
+    const handleLogout = async () => {
+        try {
+            await signOut(auth);
+        } catch (error) {
+            console.error("Error handleLogout.")
+        }
+    };
+
     return(
         <View style={styles.menu}>
             <Text style={styles.title}>Sign In</Text>
-            <TextInput 
-                style={styles.input} 
-                inputMode='email-address' 
-                placeholder='Email' 
-                value={email}
-                onChangeText={(text) => setEmail(text)}
-            />
-            <TextInput 
-                style={styles.input} 
-                secureTextEntry={true}
-                placeholder='Password'
-                value={password}
-                onChangeText={(text) => setPassword(text)}
-            />
-            <Pressable style={styles.button} onPress={handleLogin}>
-              <Text>Sign In</Text>
+            {user ? (
+            <View style={styles.logContainer}>
+                <Text style={styles.logText}>Welcome, {user.email}</Text>
+                <Text style={styles.logText}>Level: {user.level}</Text>
+                <Text style={styles.logText}>Score: {user.score}</Text>
+                <Text style={styles.logText}>Get A Letter: {user.getWord}</Text>
+                <Text style={styles.logText}>Get New Word: {user.resetGame}</Text>
+                <Text style={styles.logText}>New Word Timer: {user.newTimer}</Text>
+            <Pressable style={styles.logButton} onPress={handleLogout}>
+                <Text style={styles.logButtonText}>Log Out</Text>
             </Pressable>
-            <Pressable style={styles.button} onPress={handleSignup}>
-                <Text>Register</Text>
-            </Pressable>
+            </View>
+            ) : (
+            <View>
+                <TextInput 
+                    style={styles.input} 
+                    inputMode='email-address' 
+                    placeholder='Email' 
+                    value={email}
+                    onChangeText={(text) => setEmail(text)}
+                />
+                <TextInput 
+                    style={styles.input} 
+                    secureTextEntry={true}
+                    placeholder='Password'
+                    value={password}
+                    onChangeText={(text) => setPassword(text)}
+                />
+                <Pressable style={styles.button} onPress={handleLogin}>
+                <Text>Sign In</Text>
+                </Pressable>
+                <Pressable style={styles.button} onPress={handleSignup}>
+                    <Text>Register</Text>
+                </Pressable>
+            </View>
+            )}
         </View>
     );
 };
@@ -133,5 +163,42 @@ const styles = StyleSheet.create({
         borderColor: 'white',
         borderWidth: 1,
         borderRadius: 3,
-    }
+    },
+    logContainer: {
+        flex: 1,
+        alignItems: 'center',
+        marginTop: 20,
+        marginBottom: 20,
+        padding: 20,
+        backgroundColor: 'white',
+        borderRadius: 10,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+    },
+    logText: {
+        fontSize: 18,
+        marginBottom: 10,
+        color: '#242424',
+    },
+    logButton: {
+        backgroundColor: '#242424',
+        marginTop: 10,
+        width: '60%',
+        height: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderColor: '#242424',
+        borderWidth: 1,
+        borderRadius: 5,
+    },
+    logButtonText: {
+        color: 'white',
+        fontSize: 16,
+    },    
 });
